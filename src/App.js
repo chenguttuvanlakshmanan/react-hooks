314fc8f7ea605a8c636a23fafba82c0e898c374f
chenguttuvanlakshmanan <lchenmbss@gmail.com>
2021-06-24 14:00:39 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     <div className="container">
      <div class="item1">
           <img className="logoimg" src={logo} alt="car logo"/>
+          <p>Super Car Rocks </p>
           <button class="logo"><i>Super Cars</i> </button>
           <Navbar></Navbar>
       </div>
